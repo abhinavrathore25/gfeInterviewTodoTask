d8fbd01761dc33f6116fc2a2c12c6928b55559bd
abhin <abhinavrathore.ar@gmail.com>
2023-11-15 01:03:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [listOfCloumns, setListOfCloumns] = useState(
+    [{ name: 'Winnie', items: [] }, { name: 'Bob', items: [] }, { name: 'Thomas', items: [] }, { name: 'George', items: [] }]
+  );
+
+  useEffect(() => {
+    const localList = localStorage.getItem('listOfItems');
+    // console.log(localList);
+    if (localList !== undefined)
+      setListOfCloumns(JSON.parse(localList));
+  }, []);
+
+  const headingColors = ['#8E6E95', '#39A59C', '#344759', '#E8741E'];
+
+  function handleAdd(index) {
+
+    const task = window.prompt('Enter a task:');
+
+    let newList = JSON.parse(JSON.stringify(listOfCloumns));
+    newList[index].items.push(task);
+    localStorage.setItem('listOfItems', JSON.stringify(newList))
+    setListOfCloumns(newList);
+  }
+
+  function handleMoveTask(indexOfList, indexOfTask, task, type) {
+
+    let newList = JSON.parse(JSON.stringify(listOfCloumns));
+    newList[indexOfList].items.splice(indexOfTask, 1);
+
+    switch (type) {
+      case 'l':
+        if (indexOfList > 0)
+          newList[indexOfList - 1].items.push(task);
+        else {
+          newList[listOfCloumns.length - 1].items.push(task);
+        }
+        break;
+
+      case 'r':
+        if (indexOfList < listOfCloumns.length - 1)
+          newList[indexOfList + 1].items.push(task);
+        else {
+          newList[0].items.push(task);
+        }
+        break;
+
+      default:
+        return;
+    }
+
+
+    localStorage.setItem('listOfItems', JSON.stringify(newList))
+    setListOfCloumns(newList);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='columnContainer'>
+
+      {listOfCloumns.map((item, indexOfList) => {
+        return <div key={indexOfList} className="column">
+          <h4 style={{backgroundColor: headingColors[indexOfList]}}>{item.name}</h4>
+
+          {item.items.length !== 0 && item.items.map((task, indexOfTask) => {
+
+            return <div key={indexOfTask} className="listItem">
+              <button onClick={() => { handleMoveTask(indexOfList, indexOfTask, task, 'l') }}><i class="fa-solid fa-angle-left"></i></button>
+              {task}
+              <button onClick={() => { handleMoveTask(indexOfList, indexOfTask, task, 'r') }}><i class="fa-solid fa-angle-right"></i></button>
+            </div>
+          })}
+
+          <button onClick={() => handleAdd(indexOfList)}>+ Add a card</button>
+        </div>
+      })}
+
     </div>
   );
 }
